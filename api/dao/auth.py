0eb7f65ce22302d65a33848e9fb7b81e4d0751b0
Anthony Wynne <a.wynne@svgc.co.uk>
2022-12-21 18:24:53 +0000

    bad changes

--- api/dao/auth.py
+++ api/dao/auth.py
@@ -32,8 +32,8 @@ class AuthDAO:
     # tag::register[]
     def register(self, email, plain_password, name):
         encrypted = bcrypt.hashpw(
-            plain_password.encode("utf8"), salt=os.environ.get("SALT_ROUNDS"))  # , bcrypt.gensalt()
-        #).decode("utf8")
+            plain_password.encode("utf8"), bcrypt.gensalt()
+        ).decode("utf8")
 
         def create_user(tx, email, encrypted, name):
             return tx.run(
@@ -51,24 +51,24 @@ class AuthDAO:
                 name=name,  # (2)
             ).single()  # (3)
 
-        try:
-            with self.driver.session() as session:
-                result = session.execute_write(create_user, email, encrypted, name)
+            try:
+                with self.driver.session() as session:
+                    result = session.execute_write(create_user, email, encrypted, name)
 
-                user = result["u"]
+                    user = result["u"]
 
-                payload = {
-                    "userId": user["userId"],
-                    "email": user["email"],
-                    "name": user["name"],
-                }
+                    payload = {
+                        "userId": user["userId"],
+                        "email": user["email"],
+                        "name": user["name"],
+                    }
 
-                payload["token"] = self._generate_token(payload)
+                    payload["token"] = self._generate_token(payload)
 
-                return payload
-        except ConstraintError as err:
-            # Pass error details through to a ValidationException
-            raise ValidationException(err.message, {"email": err.message})
+                    return payload
+            except ConstraintError as err:
+                # Pass error details through to a ValidationException
+                raise ValidationException(err.message, {"email": err.message})
 
     # end::register[]
 
